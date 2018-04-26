e7854852618a339183d8171967441e172106b637
Leonardo F Costa <leonardofcosta@gmail.com>
2018-04-26 20:44:59 -0300

    new file:   app/baseClass.php 	new file:   app/endereco.php 	new file:   css/bootstrap.min.css 	new file:   css/cep.css 	new file:   css/font-awesome.min.css 	new file:   css/jquery-ui.css 	new file:   images/ui-anim_basic_16x16.gif 	new file:   index.html 	new file:   js/bootstrap.min.js 	new file:   js/cep.js 	new file:   js/jquery-1.12.4.js 	new file:   js/jquery-3.3.1.js 	new file:   js/jquery-ui.js 	new file:   js/jquery.mask.js

---
+++ js/cep.js
@@ -0,0 +1,35 @@
+
+// Autocomplete com jquery-ui
+$( function() {
+    $("#cep").autocomplete({
+      source: function( request, response ) {
+        $.ajax( {
+          url: "app/endereco.php",
+          dataType: "json",
+          data: {
+            _action: 'get_endereco', // Método remoto
+            term: request.term // Parâmetro enviado ao método
+          },
+          success: function( data ) {
+            response( data );
+          }
+        } );
+      },
+      minLength: 4,
+      select: function( event, ui ) {
+        // Alimenta os campos a partir do retorno do método remoto
+        $("#logradouro").val(ui.item.logradouro);
+        $("#cidade").val(ui.item.cidade);
+        $("#bairro").val(ui.item.bairro);
+      }
+    });
+
+});
+
+$(document).ready(function () {
+
+  var $seuCampoCpf = $("#cpf");
+
+  $seuCampoCpf.mask('000.000.000-00', {reverse: true});
+
+});
