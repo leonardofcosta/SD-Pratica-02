3d3f8339969a7c9534dcc7c079dd7e573014c5db
Leonardo F Costa <leonardofcosta@gmail.com>
2018-05-10 20:32:03 -0300

    Adicionado função get_endereco_cidade, para utilização no ajax, retorna dados do endereço.

--- app/endereco.php
+++ app/endereco.php
@@ -6,7 +6,8 @@ class endereco extends baseClass {
 
     // Lista de métodos disponíveis nesta classe
     protected $actions = array(
-        'get_endereco'
+        'get_endereco',
+        'get_endereco_cidade'
     );
 
     // Recebe o CEP como parâmetro
@@ -40,6 +41,47 @@ class endereco extends baseClass {
         tb1.cidade_descricao,
         tb2.bairro_descricao,
         tb3.endereco_logradouro
+        limit 10
+        ";
+
+        $result = $this->_select_fetch_all($sql);
+
+        // Retorna os dados em formato JSON
+        echo json_encode($result);
+
+    }
+
+    /*******************************************
+     * Adicionado em 10/05/2018
+     *
+     * Objetivo:
+     * Recebe nome da cidade como parâmetro e retorna dados de endereço
+     *
+     *******************************************/
+    public function get_endereco_cidade(){
+
+
+        $data = (object) $_GET;
+
+        $term = mysqli_real_escape_string($this->conn,$data->term);
+
+        $sql =
+        "
+        SELECT
+        e.endereco_codigo as id,
+        concat('Cidade: ', c.cidade_descricao, ' | CEP: ', ifnull(e.endereco_cep,c.cidade_cep), ' | Bairro: ', ifnull(b.bairro_descricao,'Centro'), ' | Logradouro: ', ifnull(e.endereco_logradouro,'')) as label,
+        c.cidade_descricao as value,
+        ifnull(b.bairro_descricao,'Centro') as bairro,
+        ifnull(e.endereco_logradouro,'') as logradouro,
+        ifnull(e.endereco_cep,c.cidade_cep) as cep
+        from cep.cidade c
+        left join cep.bairro b on c.cidade_codigo = b.cidade_codigo
+        left JOIN cep.endereco e on b.bairro_codigo = e.bairro_codigo
+        where c.cidade_descricao like '%$term%'
+        order by
+        c.cidade_descricao,
+        b.bairro_descricao,
+        e.endereco_logradouro
         limit 10
         ";
 
