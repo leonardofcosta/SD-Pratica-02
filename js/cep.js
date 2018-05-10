508492718a8e3fffd9820eb66e80c697e555871c
Leonardo F Costa <leonardofcosta@gmail.com>
2018-05-10 20:36:55 -0300

    Adicionado funcionalidade autocomplete para buscar cidade via ajax.

--- js/cep.js
+++ js/cep.js
@@ -13,7 +13,7 @@ $( function() {
           success: function( data ) {
             response( data );
           }
-        } );
+        });
       },
       minLength: 4,
       select: function( event, ui ) {
@@ -24,5 +24,38 @@ $( function() {
       }
     });
 
-});
 
+  /*************************************
+   * Adicionado em 10/05/2018
+   *
+   * Objetivo: 
+   * Ao digitar o nome da cidade o sistem irá autocompletar o nome da cidade
+   * oferecendo uma lista de cidades para ser selecionada, assim que selecionada 
+   * os campos CEP, Cidade, Bairro e Logradouro serão retornados nos respectivos 
+   * campos da página.
+   *
+   * Autocomplete com jquery-ui
+   *************************************/
+    $("#cidade").autocomplete({
+      source: function( request, response ) {
+        $.ajax( {
+          url: "app/endereco.php",
+          dataType: "json",
+          data: {
+            _action: 'get_endereco_cidade', // Método remoto
+            term: request.term // Parâmetro enviado ao método
+          },
+          success: function( data ) {
+            response( data );
+          }
+        });
+      },
+      minLength: 2,
+      select: function( event, ui ) {
+        // Alimenta os campos a partir do retorno do método remoto
+        $("#logradouro").val(ui.item.logradouro);
+        $("#cep").val(ui.item.cep);
+        $("#bairro").val(ui.item.bairro);
+      }
+    });
+});
